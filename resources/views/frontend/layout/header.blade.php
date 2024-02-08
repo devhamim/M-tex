078424407d0752e917a28698d20cb5b49cdd5c03
devhamim <hamim735@gmail.com>
2024-02-08 18:48:00 +0600

    update

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -11,7 +11,7 @@
                     </div>
 
                     <div id="menu" class="text-left ">
-                        <ul class="offcanvas_main_menu">
+                        <ul id="subjectsNav" style="display: grid" class="offcanvas_main_menu mt-5">
                             <li class="menu-item-has-children">
                                 <a href="{{ url('/') }}">Home</a>
                             </li>
@@ -22,16 +22,20 @@
                                 <a href="{{ route('about_us') }}">sustainability</a>
                             </li>
                             <li class="menu-item-has-children">
-                                <div class="dropdown-menu-drop">
-                                    <button class="menu-btn">Product ></button>
-                                    <div class="menu-content">
-                                        @foreach ($categories as $category)
-                                            <div class="links-hidden category">
-                                                <a href="{{ route('our.product', $category->id) }}">{{ $category->name }}</a>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
+                                <a href="#" title="SUBJECTS">Product ></a>
+                                <ul id="subnav">
+                                    @foreach ($categories as $category)
+                                    <li><a href="{{ route('our.product', $category->id) }}">{{ $category->name }}</a>
+                                        <ul>
+                                            @foreach ($subcategories as $subcat)
+                                            @if ($subcat->category_id == $category->id)
+                                                <li><a href="{{ route('our.sub.product', $subcat->id) }}">{{ $subcat->name }}</a></li>
+                                            @endif
+                                            @endforeach
+                                        </ul>
+                                    </li>
+                                    @endforeach
+                                </ul>
                             </li>
                             <li class="menu-item-has-children">
                                 <a href="{{ route('our.clients') }}">Client</a>
@@ -46,7 +50,7 @@
                     </div>
 
                     <div class="offcanvas_footer">
-                        <span><a href="#"><i class="fa fa-envelope-o"></i> info@yourdomain.com</a></span>
+                        {{-- <span><a href="#"><i class="fa fa-envelope-o"></i> info@yourdomain.com</a></span> --}}
                         <ul>
                             <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
